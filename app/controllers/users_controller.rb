5f239e2eb5508eb5755811cd9bcf6ce900eac38e
Petervl80 <pedro.lapa1911@gmail.com>
2026-04-30 13:46:26 -0300

    Build User resource

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersContorller < ApplicationController
+
+    def show
+    end
+
+    def index
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+end
